6a19173787ef6f36254c07c6b1a20a2f47a82bbb
FranForge <128065644+Frambueesaa@users.noreply.github.com>
2025-03-26 22:16:16 +0100

    - Pagina de News comenzada.

--- scripts/languageHandler.js
+++ scripts/languageHandler.js
@@ -1,18 +1,30 @@
+
+//Lenguaje del navegador.
 let language = navigator.language;
+
+//Sobreescribir el lenguaje del navegador.
+let overrideLanguage = "none";
+
 console.log(language);
 
-switch (language) {
-    case "es-ES":
-        console.log("Esta en espaniol");
-        hideLanguage("en");
-        break;
-    case "en-EN":
-        console.log("Esta en ingles");
-        hideLanguage("es");
-        break;
-    default:
-        console.log("Esta en otro idioma");
-        break;
+if (overrideLanguage == "none") {
+    switch (language) {
+        case "es-ES":
+            console.log("Esta en espaniol");
+            hideLanguage("en");
+            break;
+        case "en-EN":
+            console.log("Esta en ingles");
+            hideLanguage("es");
+            break;
+        default:
+            console.log("Esta en otro idioma");
+            hideLanguage("es");
+            break;
+    }
+}
+else {
+    console.log("Se esta sobreescribiendo el idioma de la pagina.");
 }
 
 //Oculta un lenguaje en especifico.
