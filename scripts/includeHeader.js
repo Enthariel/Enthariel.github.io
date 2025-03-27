b2b0326d841fc55e6fd532781b53dc432ef0282d
FranForge <128065644+Frambueesaa@users.noreply.github.com>
2025-03-27 19:37:39 +0100

    - Header con .js

--- scripts/includeHeader.js
+++ scripts/includeHeader.js
@@ -1,1 +1,25 @@
-document.getElementById("header").innerHTML = "<p>ENTHARIEL GAMES</p>";
+document.getElementById("header").innerHTML = `
+    <a href="#start" class="logoDegree"></a>
+    <a href="#news" class="mobileHidden">
+        <p>NOTICIAS</p>
+    </a>
+    <a href="#games" class="mobileHidden">
+        <p>JUEGOS</p>
+    </a>
+    <!-- TWITTER -->
+    <a href="#" class="mobileHidden">
+        <img src="images/icons/Twitter.svg" alt="Twitter" class="social" />
+    </a>
+    <!-- INSTAGRAM -->
+    <a href="#" class="mobileHidden">
+        <img src="images/icons/Instagram.svg" alt="Instagram" class="social" />
+    </a>
+    <!-- LINKEDIN -->
+    <a href="#" class="mobileHidden">
+        <img src="images/icons/Linkedin.svg" alt="LinkedIn" class="social" />
+    </a>
+    <!-- YOUTUBE -->
+    <a href="#" class="mobileHidden">
+        <img src="images/icons/Youtube.svg" alt="YouTube" class="social" />
+    </a>
+`;
